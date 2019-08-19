5f11a9b98828dcf8146f1667fc1d316132120d83
Oliver Steiner <oliver@mollo.ch>
2019-08-19 21:57:28 +0200

    shell-script for copy distro to destination

---
+++ drullo.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# add dist to drullo
+#
+# requires:  npm install -g open
+
+URL='https://drullo.local/admin/newsletter/analytics';
+DRULLO='/Volumes/Aktuell/MAMP/drullo/web/';
+MODULE='modules/custom/smmg_newsletter/';
+ANALYTICS=${DRULLO}${MODULE}'analytics';
+
+echo ''
+echo '------------------------'
+echo 'copy dist to drullo...'
+echo ''
+
+# app.js
+echo '  -- js'
+cp  ./dist/js/* ${ANALYTICS};
+
+# app.css
+echo '  -- css'
+cp  ./dist/css/* ${ANALYTICS};
+
+
+FILE=${ANALYTICS}'/app.js'
+if test -f "$FILE"; then
+    echo ''
+    echo "done."
+fi
+echo '------------------------'
+echo ''
+open ${URL}
