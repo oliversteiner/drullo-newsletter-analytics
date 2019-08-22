1a16d1d0110ebf076cb63b502e9889f8c34fb0e0
Oliver Steiner <oliver@mollo.ch>
2019-08-22 18:24:37 +0200

    install plugin vue-cli-typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,7 @@
 module.exports =  {
-  parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
+  // Specifies the ESLint parser
+  parser:  '@typescript-eslint/parser',
+
   extends:  [
     "plugin:vue/essential",
     "@vue/standard",
@@ -7,14 +9,25 @@ module.exports =  {
     'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
+
   parserOptions:  {
-    ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
-    sourceType:  'module',  // Allows for the use of imports
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser'
   },
+
   rules:  {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
   "indent": ["error", 2]
 },
+
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard',
+    '@vue/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ]
 };
 
