f6fc2f5c8a51ca017c0682446930ba74a8498ea5
Oliver Steiner <oliver@mollo.ch>
2019-09-03 09:35:11 +0200

    add helper : getSubscriberStatus

---
+++ src/_helper/member.ts
@@ -0,0 +1,51 @@
+import { Member, MolloMemberData, SubscriberStatus } from '@/store/models'
+import { EnumsSubscriberStatus } from '@/enums'
+
+/**
+ *
+ * STATUS:
+ *  - none -> white outline
+ *  - send -> white full
+ *  - open -> green
+ *  - error -> red ( email-address invalide)
+ *  - unsubscribe -> viollet
+ *  - warning -> orange (not definded)
+ * @param data
+ */
+export default function getSubscriberStatus(data: MolloMemberData[]): SubscriberStatus[] {
+  const status: SubscriberStatus[] = []
+
+  // no Data
+  if (!data) return status
+
+  // Data
+  data.forEach(data => {
+
+    console.log('data', data);
+
+    const message = {
+      messageId: 0,
+      // Default
+      status: EnumsSubscriberStatus.NONE,
+    }
+
+    // Add Message ID
+    if (data.messageId) {
+      message.messageId = data.messageId
+    }
+
+    // Compute Status
+    // Send
+    if (data.sendTS != 0) message.status = EnumsSubscriberStatus.SEND
+    if (data.send) message.status = EnumsSubscriberStatus.SEND
+
+    // Open
+    if (data.open) message.status = EnumsSubscriberStatus.OPEN
+    // Unsubscribe
+    if (data.unsubscribe) message.status = EnumsSubscriberStatus.UNSUBSCRIBE
+
+    status.push(message)
+  })
+
+  return status
+}
