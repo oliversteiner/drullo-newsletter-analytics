b265bb90611f18ca2fde8ed93d94524ae1948351
Oliver Steiner <oliver@mollo.ch>
2019-09-06 23:54:56 +0200

    clean up

--- src/components/SubscriberTable/SubscriberTable.vue
+++ src/components/SubscriberTable/SubscriberTable.vue
@@ -62,19 +62,12 @@ export default class SubscriberTable extends Vue {
 
   get rows() {
     const rows: any[] = []
-    const rowsSample = [
-      { id: 1, name: 'John', age: 20, createdAt: '', score: 0.03343 },
-      { id: 2, name: 'Jane', age: 24, createdAt: '2011-10-31', score: 0.03343 },
-      { id: 3, name: 'Susan', age: 16, createdAt: '2011-10-30', score: 0.03343 },
-      { id: 4, name: 'Chris', age: 55, createdAt: '2011-10-11', score: 0.03343 },
-      { id: 5, name: 'Dan', age: 40, createdAt: '2011-10-21', score: 0.03343 },
-      { id: 6, name: 'John', age: 20, createdAt: '2011-10-31', score: 0.03343 },
-    ]
+
     this.subscriberList.map((subscriber: Subscriber) => {
       let row = {
         id: subscriber.id,
         firstName: subscriber.address.first_name,
-        last_name: subscriber.address.last_name,
+        lastName: subscriber.address.last_name,
       }
       rows.push(row)
     })
