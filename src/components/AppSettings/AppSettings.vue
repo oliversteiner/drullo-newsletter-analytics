72d8ddc5fbf6cd23a02348e183578743630170f7
Oliver Steiner <oliver@mollo.ch>
2019-10-02 20:44:12 +0200

    new Component AppSettings

---
+++ src/components/AppSettings/AppSettings.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app-settings">
+    <div class="icon">
+      <font-awesome-icon icon="cog"></font-awesome-icon>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component({})
+export default class AppSettings extends Vue {}
+</script>
+
+<style lang="scss">
+@import 'AppSettings';
+</style>
